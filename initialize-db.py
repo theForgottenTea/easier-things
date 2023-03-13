cf691a883c90a08a3075350f5678d35a059bcaf6
Tea <103472961+theForgottenTea@users.noreply.github.com>
2023-03-12 21:43:25 -0600

    Create initialize-db.py

---
+++ initialize-db.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+include sqlite3
+#
+#load cosmetics from a configuration file
+#
+#get working directory
+#
+#check working directory for any db file, if no file then create file
+#
+#if file do not create db and simply open connection on present db file
+#
+#start doing sql things
