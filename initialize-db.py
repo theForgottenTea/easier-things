f009f2c6a3d25b2a4adf556e3076883aed181e11
Tea <103472961+theForgottenTea@users.noreply.github.com>
2023-03-15 14:31:30 -0600

    Update initialize-db.py

--- initialize-db.py
+++ initialize-db.py
@@ -4,47 +4,25 @@ include sqlite3
 #load cosmetics from a configuration file***********************
 #
 confile=open("config.txt", "r", encoding="utf-8")
+readed =confile.read()
 conflist=[]
-#start appending conflist with stream data************
-#get working directory********************************
-#
-#dont be a fool
+conflist.append(readed)
+#dont be a fool<<<<<<<<<<
 confile.close()
-#dont be a fool
-#
-#eventually a list will be used... so here.
+#dont be a fool<<<<<<<<<<
 somelist=[]
 connection = sqlite3.connect("test.db")
 sqlcursor = connection.cursor()
 #
 #
-#i assume execute only takes string info to run sql things on... so we use string (:
-#args should eventually be variable taking input as the user wishes to use or to be taken from********
-#some configuration file defining common tasks to do
 args = ''
-#
-#
 #sqlite data types for py will be
 #string = text
 #int = integer
 #float = real
 #binary = blob
-#
 sqlcursor.execute("create table testTable(year integer, titles string)")
-# ^^^^^ start passing commands
-#
-#dont sql inject pls. do a batch insert (for speed, zoom!)
-#each "?" is a placeholder for eventual list data to be put into the tables.
-#the table only has 2 columns in it so 2 ? should be used to place the things that should pop
-#those columns
 argline.executemany("insert into testTable values(?,?)",someList)
-#
-#check working directory for any db file, if no file then create file**********************
-#
-#if file do not create db and simply open connection on present db file*******************
-#
-#start doing sql things**********************************
-#
 #dont be a fool<<<<<<<
 connection.close()
 #dont be a fool<<<<<<<
